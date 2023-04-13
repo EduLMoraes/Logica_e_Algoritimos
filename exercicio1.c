c76362a280d5dfb479b18117a997006aae3af734
eduardomoreaes <17eduardo05@gmail.com>
2023-04-13 20:01:16 -0300

    modified:   exercicio1.c 	modified:   exercicio2 	modified:   exercicio2.c
    	modified:   exercicio1

--- exercicio1.c
+++ exercicio1.c
@@ -12,8 +12,19 @@ int main(){
     printf("Informe os pesos na ordem em que informou as notas: \n");
     scanf("%i", &peso1);
     scanf("%i", &peso2);
+    while (!mp)
+    {
+    if(peso1+peso2 == 10){ 
+        mp = ((peso1*nota1) + (peso2*nota2))/(peso1+peso2);
+    }else{
+        printf("Informe os pesos de forma que a soma deles de 10! Insira-os novamente\n");
+        scanf("%i", &peso1);
+        scanf("%i", &peso2);
+        }
 
-    mp = ((peso1*nota1) + (peso2*nota2))/(peso1+peso2);
+    }
 
-    printf("A Média Ponderada foi: %f", mp);
+
+
+    printf("A Média Ponderada foi: %.2f", mp);
 }
--- exercicio2.c
+++ exercicio2.c
@@ -7,7 +7,14 @@ int main(){
     scanf("%f", &n2);
     scanf("%f", &n3);
 
-    mh = ((1/n1) + (1/n2) + (1/n3))/3;
+    if (n1 != 0 && n2 != 0 && n3 != 0)
+    {
+        mh = ((1/n1) + (1/n2) + (1/n3))/3;
+        
+    }else{
+        mh = 0;
+    }
+    
 
-    printf("Média Harmonica: %f", mh);
+    printf("Média Harmonica: %.2f", mh);
 }
